bddc8795983eff104a0e923fdb861afa4a027804
Polina Balazhy <pbalazhy@stud.khpi.ucode-connect.study>
2022-04-30 21:04:09 +0300

    cards' initialization added

--- models/init.js
+++ models/init.js
@@ -1,24 +1,24 @@
 import mysql from "mysql2/promise";
 import db from "./index.js";
 
-// const Card = db.sequelize.models.card; 
+const Card = db.sequelize.models.card; 
 
-// async function initializeCards() {
+async function initializeCards() {
 
-//     await Card.bulkCreate([
-//         { alias: "Thor", attack_points: 150, defense_points: 100, cost: 5 },
-//         { alias: "Iron Man", attack_points: 130, defense_points: 90, cost: 5 },
-//         { alias: "Spider Man", attack_points: 140, defense_points: 80, cost: 5 },
-//         { alias: "Thanos", attack_points: 100, defense_points: 90, cost: 5 },
-//         { alias: "Captain America", attack_points: 130, defense_points: 100, cost: 5 },
-//         { alias: "Black Widow", attack_points: 120, defense_points: 90, cost: 5 },
-//         { alias: "Hulk", attack_points: 130, defense_points: 120, cost: 5 },
-//         { alias: "Rocket", attack_points: 100, defense_points: 80, cost: 5 },
-//         { alias: "Gamora", attack_points: 130, defense_points: 90, cost: 5 },
-//         { alias: "Superman", attack_points: 100, defense_points: 110, cost: 5 },
-//     ]);
+    await Card.bulkCreate([
+        { alias: "Thor", attack_points: 150, defense_points: 100, cost: 5 },
+        { alias: "Iron Man", attack_points: 130, defense_points: 90, cost: 5 },
+        { alias: "Spider Man", attack_points: 140, defense_points: 80, cost: 5 },
+        { alias: "Thanos", attack_points: 100, defense_points: 90, cost: 5 },
+        { alias: "Captain America", attack_points: 130, defense_points: 100, cost: 5 },
+        { alias: "Black Widow", attack_points: 120, defense_points: 90, cost: 5 },
+        { alias: "Hulk", attack_points: 130, defense_points: 120, cost: 5 },
+        { alias: "Rocket", attack_points: 100, defense_points: 80, cost: 5 },
+        { alias: "Gamora", attack_points: 130, defense_points: 90, cost: 5 },
+        { alias: "Superman", attack_points: 100, defense_points: 110, cost: 5 },
+    ]);
 
-// }
+}
 
 export default async function initialize() {
 
@@ -28,6 +28,10 @@ export default async function initialize() {
 
     await db.sequelize.sync();
 
-    // await initializeCards();
+    let existsQuery = `EXISTS (SELECT * FROM ${database}.cards)`;
+    let cards = await conn.query(`SELECT ${existsQuery}`);
+    
+    if (!cards[0][0][existsQuery])
+        await initializeCards();
 
 }
